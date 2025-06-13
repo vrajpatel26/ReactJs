ea4c0482c161b79ed53c37d1b739bad7454af1c6
vrajpatel26 <vraj.26112004@gmail.com>
2025-06-13 12:09:19 +0530

    updare

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,3 +1,15 @@
 export const App = () =>{
- return <h1>Hello , Vraj Patel</h1>
+//  return <h1>Hello , Vraj Patel</h1>
+
+return (
+    <div>
+        <div>
+            <img src="MONEY-HEIST.jpg" alt="MONEY-HEIST" height="40%" width="40%" />
+            <h2>Name: Money Heist</h2>
+            <h3>Rating: 9.3</h3>
+            <p>Summary: Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex nesciunt modi amet dolore perspiciatis. Temporibus labore laudantium ratione tempora ab!</p>
+        </div>
+
+    </div>
+)
 }
