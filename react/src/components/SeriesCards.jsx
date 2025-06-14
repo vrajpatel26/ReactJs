6e4af6ecbf0461530c05951d26de0c80927654ac
vrajpatel26 <vraj.26112004@gmail.com>
2025-06-15 00:58:19 +0530

    update

--- react/src/components/SeriesCards.jsx
+++ react/src/components/SeriesCards.jsx
@@ -1,6 +1,21 @@
 export const SeriesCards = (props) => {
 
     const { img_url, name, rating, description, cast, genre, watch_url } = props.data //destructuring
+
+    const btn_style = {
+        padding: "1.2rem",
+        border: "none",
+        fontSize: "1.5rem",
+        backgroundColor: `${rating >= 8.5 ? " rgb(113, 221, 113)" : "rgb(207, 207, 58)"}`,
+        color: "black",
+        fontWeight:"bold",
+        cursor:"pointer"
+    }
+
+    const ratingColor = rating >= 8.5 ? "super-hit" : "average";
+
+    const btnColor = rating >= 8.5 ? "super-hit" : "average";
+
     return (
         <li className="card">
             <div>
@@ -12,12 +27,15 @@ export const SeriesCards = (props) => {
             </div>
             <div className="card-content">
                 <h2>Name: {name}</h2>
-                <h3>Rating: {rating}</h3>
+                {/* <h3 style={{padding :"0.6rem 0rem"}}>Rating: {rating}</h3> */}
+                {/* <h3>Rating: <span className={` rating ${rating >= 8.5 ? "super-hit" : "average"}`}> {rating} </span> </h3> */}
+                <h3>Rating: <span className={` rating ${ratingColor}`}> {rating} </span> </h3>
+
                 <p>Summary: {description}</p>
-                <p>Genre : {genre}</p>
-                <p>Cast : {cast}</p>
+                <p>Genre : {genre.join(", ")}</p>
+                <p>Cast : {cast.join(", ")}</p>
                 <a href={watch_url} target="_">
-                    <button>Watch Now</button>
+                    <button style={btn_style}>Watch Now</button>
                 </a>
             </div>
         </li>
