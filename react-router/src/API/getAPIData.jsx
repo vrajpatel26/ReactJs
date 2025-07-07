46c45774bc315fc090d570dfe89998a9008ac846
vrajpatel26 <vraj.26112004@gmail.com>
2025-07-07 21:52:01 +0530

    update

--- react-router/src/API/getAPIData.jsx
+++ react-router/src/API/getAPIData.jsx
@@ -1,6 +1,7 @@
 export const getMoviesData = async () => {
     try {
-        const response = await fetch("https://www.omdbapi.com/?i=tt3896198&apikey=1c12799f&s=titanic&page=1")
+        const response = await fetch(`https://www.omdbapi.com/?i=tt3896198&apikey=${import.meta.env.VITE_API_KEY}&s=titanic&page=1`)
+        // const response = await fetch(`https://www.omdbapi.com/?i=tt3896198&apikey= 1c12799f&s=titanic&page=1`)
         const data = response.json()
         return data;
     } catch (error) {
--- react-router/src/components/layout/AppLayout.jsx
+++ react-router/src/components/layout/AppLayout.jsx
@@ -1,8 +1,16 @@
-import { Outlet } from "react-router-dom"
+import { Outlet, useNavigate, useNavigation } from "react-router-dom"
 import { Footer } from "./Footer"
 import { Header } from "./Header"
+import { Loading } from "./Loading"
 
 export const AppLayout = ()=>{
+
+    const navigation = useNavigation()
+
+    if(navigation === "loading") return <Loading />
+
+
+
     return(
         <>
         <Header />
